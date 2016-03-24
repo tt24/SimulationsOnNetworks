f0d4360d6b5f984da72648730cfe44ac0bd1f828
tt24 <tt24@st-andrews.ac.uk>
2016-03-24 11:03:58 +0000

    experiments

--- ExperimentRunner/src/Main.java
+++ ExperimentRunner/src/Main.java
@@ -3,12 +3,12 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		int numOfExecutions = 50;
-		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("seir-experiment1.csv")));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("experiment10.csv")));
 		bw.write("pInfected, gamma, beta, eta, N, elapsed_time, timesteps, events, timesteps_with_events, mean_outbreak_size, max_outbreak_size, max_outbreak_proportion\n");
 		bw.close();
 		for (int n = 0; n < numOfExecutions; n++) {
 			Runtime rt = Runtime.getRuntime();
-			Process pr = rt.exec("python ../../SEIRmain.py");
+			Process pr = rt.exec("python ../../SEIDRmain.py");
 			System.out.println(pr.waitFor());
 		}
 	}
