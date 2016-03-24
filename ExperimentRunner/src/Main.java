b7b05f083b37f5cc4a46018df8b7d8ad217c3db5
tt24 <tt24@st-andrews.ac.uk>
2016-03-24 14:26:22 +0000

    experiments@

--- ExperimentRunner/src/Main.java
+++ ExperimentRunner/src/Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		int numOfExecutions = 50;
-		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("experiment10.csv")));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("experiment15.csv")));
 		bw.write("pInfected, gamma, beta, eta, N, elapsed_time, timesteps, events, timesteps_with_events, mean_outbreak_size, max_outbreak_size, max_outbreak_proportion\n");
 		bw.close();
 		for (int n = 0; n < numOfExecutions; n++) {
--- SEIDRmain.py
+++ SEIDRmain.py
@@ -662,7 +662,7 @@ def show_changes(G, syn_dyn):
 
 # In[8]:
 
-delta = 0.389
+delta = 0.589
 epsilon = 0.2
 zeta = 0.5
 household_size =5
@@ -676,7 +676,7 @@ p_edge_creation = 0.002
 # we do  not distinguish between suspected and probable cases, so take the average
 syn = SEIDRSynchronousDynamics(pInfected = 0.00136557, pExposed = 0.0,
                                           beta = 0.1151, gamma = 0.06851662, eta = 0.083333, 
-                                          delta =delta, epsilon = epsilon, zeta = zeta, g = nx.erdos_renyi_graph(number_of_nodes, p_edge_creation), rewire_degree=0.0)
+                                          delta =delta, epsilon = epsilon, zeta = zeta, g = nx.erdos_renyi_graph(number_of_nodes, p_edge_creation), rewire_degree=0.15)
 syn_dyn = syn.dynamics()
 
 
@@ -685,7 +685,7 @@ syn_dyn = syn.dynamics()
 import io
 import os
 SEPARATOR = ', '
-file_num = 10
+file_num = 16
 if os.path.isfile('experiment'+str(file_num)+'.csv'):
     file = open('experiment'+str(file_num)+'.csv', 'a')
 else:
