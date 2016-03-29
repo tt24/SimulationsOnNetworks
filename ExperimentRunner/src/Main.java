5df0d2bd47ecc92f9ca7ac2b60afbb871f91f80e
tt24 <tt24@st-andrews.ac.uk>
2016-03-29 21:22:16 +0100

    more automisation

--- ExperimentRunner/src/Main.java
+++ ExperimentRunner/src/Main.java
@@ -1,18 +1,33 @@
 import java.io.*;
-
+import java.text.DecimalFormat;
 public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		int numOfExecutions = 50;
-		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("seir-experiment-influenza1.1.csv")));
-//		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("experiment-beta9.2.csv")));
-//		bw.write("p_edge_creation,p_infected,gamma,beta,delta,epsilon,zeta,eta,N,elapsed_time,timesteps,events,timesteps_with_events,mean_outbreak_size,max_outbreak_size,max_outbreak_proportion,exposed_from_infected,exposed_from_dead,rewire_degree\n");
-		bw.write("p_edge_creation,p_infected,gamma,beta,eta,N,elapsed_time,timesteps,events,timesteps_with_events,mean_outbreak_size,max_outbreak_size,max_outbreak_proportion,exposed_from_infected\n");
-		bw.close();
-		for (int n = 0; n < numOfExecutions; n++) {
-			Runtime rt = Runtime.getRuntime();
-//			Process pr = rt.exec("python ../../SEIDRmain.py");
-			Process pr = rt.exec("python ../../SEIRinfluenza.py");
-			System.out.println(pr.waitFor());
+		DecimalFormat df = new DecimalFormat("#.###");
+		int fileNum = 8;
+		int verNum = 2;
+		double rewire = 0.15;
+		double beta = 0.3151;
+		while(rewire>=0.0) {
+			double delta = 0.589;
+			while(delta>=0.289) {
+				System.out.println("python ../../SEIDRmain.py " +df.format(delta)+" "+beta+" "+rewire+" "+fileNum+ " "+verNum);
+				BufferedWriter bw = new BufferedWriter(new FileWriter(new File("experiment-beta"+fileNum+"."+verNum+".csv")));
+		//		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("experiment-beta9.2.csv")));
+				bw.write("p_edge_creation,p_infected,gamma,beta,delta,epsilon,zeta,eta,N,elapsed_time,timesteps,events,timesteps_with_events,mean_outbreak_size,max_outbreak_size,max_outbreak_proportion,exposed_from_infected,exposed_from_dead,rewire_degree\n");
+		//		bw.write("p_edge_creation,p_infected,gamma,beta,eta,N,elapsed_time,timesteps,events,timesteps_with_events,mean_outbreak_size,max_outbreak_size,max_outbreak_proportion,exposed_from_infected\n");
+				bw.close();
+				for (int n = 0; n < numOfExecutions; n++) {
+					Runtime rt = Runtime.getRuntime();
+					Process pr = rt.exec("python ../../SEIDRmain.py " +df.format(delta)+" "+beta+" "+rewire+" "+fileNum+ " "+verNum);
+		//			Process pr = rt.exec("python ../../SEIRinfluenza.py");
+					System.out.println(pr.waitFor());
+				}
+				delta=delta-0.1;
+				fileNum-=1;
+			}
+			rewire=rewire-0.15;
 		}
+
 	}
 }
