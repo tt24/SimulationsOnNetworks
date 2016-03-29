4b6f81b1c220de7b9888f89e6d75ea72e5c42ee7
tt24 <tt24@st-andrews.ac.uk>
2016-03-29 20:45:09 +0100

    experiments

--- ExperimentRunner/src/Main.java
+++ ExperimentRunner/src/Main.java
@@ -3,12 +3,15 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		int numOfExecutions = 50;
-		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("experiment-beta9.2.csv")));
-		bw.write("p_edge_creation,p_infected,gamma,beta,delta,epsilon,zeta,eta,N,elapsed_time,timesteps,events,timesteps_with_events,mean_outbreak_size,max_outbreak_size,max_outbreak_proportion,exposed_from_infected,exposed_from_dead,rewire_degree\n");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("seir-experiment-influenza1.1.csv")));
+//		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("experiment-beta9.2.csv")));
+//		bw.write("p_edge_creation,p_infected,gamma,beta,delta,epsilon,zeta,eta,N,elapsed_time,timesteps,events,timesteps_with_events,mean_outbreak_size,max_outbreak_size,max_outbreak_proportion,exposed_from_infected,exposed_from_dead,rewire_degree\n");
+		bw.write("p_edge_creation,p_infected,gamma,beta,eta,N,elapsed_time,timesteps,events,timesteps_with_events,mean_outbreak_size,max_outbreak_size,max_outbreak_proportion,exposed_from_infected\n");
 		bw.close();
 		for (int n = 0; n < numOfExecutions; n++) {
 			Runtime rt = Runtime.getRuntime();
-			Process pr = rt.exec("python ../../SEIDRmain.py");
+//			Process pr = rt.exec("python ../../SEIDRmain.py");
+			Process pr = rt.exec("python ../../SEIRinfluenza.py");
 			System.out.println(pr.waitFor());
 		}
 	}
